7cf5e94b3347905fba038809ebfdd0f1c1d21023
Sarah Richens <sarahgrichens@gmail.com>
2023-04-23 17:43:32 -0500

    cleaned up js and added comments

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,33 +13,35 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 
 export const putDb = async (content) => {
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
+  //*worked on the following with tutor*
+  //add id of 1 so that's it just overwrites when new content is added, rather than add a new object each time
+  //Also will ensure offline data saves, since it's all referring back to the same content  
   const request = store.put({ id: 1, content });
   const result = await request;
   console.log('🚀 - data saved to the database', result);
 };
 
-//console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.get(1);
+  const store = tx.objectStore('jate');  
+  const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
+  //*with tutor* added .content so that it will load database data and local storage data without an error 
+  //message referring to .split, since that will only work on strings
+  //added the ? promise so we won't get an error message if there isn't already data in the database
   return result?.content;
 };
 
 
-//console.error('getDb not implemented');
 
 initdb();
